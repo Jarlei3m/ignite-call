8eb2bda73fbc67dcbf64e9a0ad487b1e7b8ced01
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2023-01-16 19:38:06 -0300

    feat: Add react-hook-form

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,11 +1,23 @@
 import { Button, TextInput } from "@ignite-ui/react";
 import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
 import { Form } from "./styles";
 
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm();
+
+  async function handleClaimUsername(data: any) {
+    console.log("data", data);
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-nome" />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-nome"
+        {...register("username")}
+      />
       <Button size="sm" type="submit">
         Reservar
         <ArrowRight />
