411effc0f69e0049dc9e8c7dbebee6be8ebcde98
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2023-01-16 07:10:35 -0300

    chore: Add eslint config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import { Heading } from '@ignite-ui/react'
 
-export default function Home() {  
-  return (
-    <Heading as='h1'>Hello world</Heading>
-  )
+export default function Home() {
+  return <Heading as="h1">Hello world</Heading>
 }
